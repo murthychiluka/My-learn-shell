8d4a3671949edcaee50e122fbc88e7c77f79f83c
Murthy <murthychiluka@gmail.com>
2023-03-14 14:00:33 +0530

    a

---
+++ celcius.sh
@@ -0,0 +1,9 @@
+# Define the function
+celsius_to_fahrenheit() {
+    celsius=$1
+    fahrenheit=$(echo "scale=2; ($celsius * 1.8) + 32" | bc)
+    echo "$celsius degrees Celsius is equal to $fahrenheit degrees Fahrenheit"
+}
+
+# Call the function with an argument of 25 degrees Celsius
+celsius_to_fahrenheit 25
