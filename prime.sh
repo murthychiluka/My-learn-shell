f19f94d396d844f2ab8fd5a49a5b6afc978af566
Murthy <murthychiluka@gmail.com>
2023-03-14 14:11:22 +0530

    a

--- prime.sh
+++ prime.sh
@@ -14,7 +14,7 @@ is_prime() {
 
 # Call the function with an argument of 17
 is_prime 17
-These are just a few examples of how you can use functions in shell scripts. Functions can be very useful for organizing your code and making it easier to read and maintain.
+#These are just a few examples of how you can use functions in shell scripts. Functions can be very useful for organizing your code and making it easier to read and maintain.
 
 
 
