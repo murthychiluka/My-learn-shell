e79aeb6ac14e5e1053b5be0eb1f4d92547b4fc6a
murthychiluka <murthychiluka@gmail.com>
2023-06-29 15:27:12 +0200

    d

--- if_with_AND.sh
+++ if_with_AND.sh
@@ -2,7 +2,7 @@ echo 'Enter username'
 read username
 echo 'Enter password'
 read password
-if [[ ( $username == 'admin' && $password == admin123 )]];
+if [ ( $username == 'admin' && $password == admin123 )];
 then
 echo 'valid user'
 else
