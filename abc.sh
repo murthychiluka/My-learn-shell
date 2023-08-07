50d600fb04ac9319e77a66e5db844afb1ccb6602
murthychiluka <murthychiluka@gmail.com>
2023-08-07 19:53:35 +0200

    du

--- abc.sh
+++ abc.sh
@@ -7,3 +7,14 @@ echo "Enter your number: "
 read no
 
 echo $no + $length
+
+drwxr-xr-x  2 user user  4.0K Aug  1 10:00 dir1
+-rw-r--r--  1 user user  2.0K Aug  1 09:00 file1.txt
+-rw-r--r--  1 user user  5.0K Aug  1 08:00 file2.txt
+
+#After running du -sch*
+
+4.0K	dir1
+2.0K	file1.txt
+5.0K	file2.txt
+11K	total
